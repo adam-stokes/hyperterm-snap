c58cfe8f7042abea883fc9dd6d0af11b02b53919
Adam Stokes <adam.stokes@ubuntu.com>
2016-07-26 08:37:10 -0400

    init, doesnt work yet
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

---
+++ parts/plugins/hypertermpkg.py
@@ -0,0 +1,44 @@
+# -*- Mode:Python; indent-tabs-mode:nil; tab-width:4 -*-
+#
+# Copyright (C) 2015-2016 Canonical Ltd
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""The nodejs plugin is useful for node/npm based parts.
+
+The plugin uses node to install dependencies from `package.json`. It
+also sets up binaries defined in `package.json` into the `PATH`.
+
+This plugin uses the common plugin keywords as well as those for "sources".
+For more information check the 'plugins' topic for the former and the
+'sources' topic for the latter.
+
+Additionally, this plugin uses the following plugin-specific keywords:
+
+    - node-packages:
+      (list)
+      A list of dependencies to fetch using npm.
+    - node-engine:
+      (string)
+      The version of nodejs you want the snap to run on.
+"""
+import snapcraft
+from snapcraft.plugins import nodejs  # noqa
+
+
+class HypertermPkgPlugin(snapcraft.plugins.nodejs.NodePlugin):
+
+    def build(self):
+        super().build()
+        self.run(['npm', 'install', '--only=development'])
+        self.run(['npm', 'run', 'pack'])
